d1ce6e65ca8cbeb5038293be686ef0fede19dff7
earlgrey07 <earl_grey07@icloud.com>
2024-05-29 00:27:23 +0300

    Initial Commit

---
+++ ProfileCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileCard
+//
+//  Created by Данил on 29.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
